736ddb5b98515c08e52e4ac8227eb9be6d2eb145
Amogh Shetty <amoggo2004@gmail.com>
2025-03-30 18:57:56 -0700

    Modify Navbar styling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-black shadow-md">
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-black/40 shadow-md">
       <div className="container mx-auto px-6 py-4 flex items-center justify-between">
         <div className="flex items-center space-x-3">
           {/* Replace with your actual logo in public/logo.svg */}
           <img
-            src="/logo.svg"
+            src="/logo.jpg"
             alt="Devils Ad Astra Logo"
             className="h-10 w-10"
           />
